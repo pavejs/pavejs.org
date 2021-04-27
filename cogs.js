98c02018e0326464412351af68092d1785d3636d
Nathaniel Pendy <nathanielp2402@gmail.com>
2021-04-27 09:16:57 -0500

    More work on polishing initially setup crappy env into usable env

---
+++ cogs.js
@@ -0,0 +1,97 @@
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import colors from 'tailwindcss/colors.js';
+
+const { env } = process;
+
+const MINIFY = env.MINIFY === '1';
+
+export default {
+  main: {
+    transformers: [].concat(
+      { name: 'underscore-template', only: ['src/**/*.+(conf|html)'] },
+      {
+        name: 'babel',
+        only: [
+          'src/**/*.js',
+          'node_modules/+(formatted-text|pave|yaml)/**/*.js',
+          '**/*.+(json|svg)'
+        ],
+        options: {
+          caller: { name: 'cogs', supportsDynamicImport: true },
+          plugins: [
+            '@babel/plugin-proposal-class-properties',
+            ['babel-plugin-styled-components', { displayName: !MINIFY }]
+          ],
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: [
+                    'last 2 Chrome versions',
+                    'last 2 ChromeAndroid versions',
+                    'last 2 Edge versions',
+                    'last 2 Firefox versions',
+                    'last 2 iOS versions',
+                    'last 2 Safari versions'
+                  ]
+                }
+              }
+            ],
+            [
+              '@babel/preset-react',
+              { development: !MINIFY, runtime: 'automatic' }
+            ]
+          ]
+        }
+      },
+      {
+        name: 'postcss',
+        only: 'src/index.css',
+        options: {
+          plugins: [
+            tailwindcss({
+              mode: 'jit',
+              purge: ['src/*'],
+              theme: {
+                extend: {
+                  colors: {
+                    cyan: colors.cyan,
+                    orange: {
+                      50: '#ffffb4',
+                      100: '#ffa650',
+                      200: '#ff923c',
+                      300: '#ff7e28',
+                      400: '#ff6a14',
+                      500: '#ff5600',
+                      600: '#eb4200',
+                      700: '#d72e00',
+                      800: '#c31a00',
+                      900: '#af0600'
+                    }
+                  }
+                }
+              }
+            }),
+            autoprefixer
+          ]
+        }
+      },
+      {
+        only: 'src/index.css',
+        fn: ({ file: { links } }) => ({ links: [...links, 'src/**/*'] })
+      },
+      MINIFY ? { name: 'csso', only: 'src/index.css' } : []
+    ),
+    builds: {
+      'src/**/*': { base: 'src/public', dir: 'public' },
+      'src/**/*.js': {
+        base: 'src/pages',
+        dir: 'public',
+        ext: { '.js': '.html' }
+      },
+      'src/index.css': { base: 'src', dir: 'public' }
+    }
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import loadLivereload from './load-livereload.js';
+import reportWebVitals from './report-web-vitals';
+import Root from './root';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +16,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+loadLivereload();
