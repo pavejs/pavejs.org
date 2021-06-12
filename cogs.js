ba474877eb6520d6739886f9cdf296b9ed2b4cd8
nathanielop <nathaniel@galactivity.com>
2021-06-12 12:34:57 -0500

    CRLF -> LF b/c fk Windows, adjustments to cogs and other things

--- cogs.js
+++ cogs.js
@@ -7,80 +7,98 @@ const { env } = process;
 
 const MINIFY = env.MINIFY === '1';
 
-export default {
-  main: {
-    transformers: [].concat(
-      { name: 'underscore-template', only: ['src/**/*.+(conf|html)'] },
-      {
-        name: 'replace',
-        only: 'node_modules/**/*.js',
-        options: {
-          flags: 'g',
-          patterns: {
-            'process\\.env\\.NODE_ENV': MINIFY
-              ? '"production"'
-              : '"development"'
-          }
-        }
-      },
-      {
-        name: 'babel',
-        only: ['src/**/*.js'],
-        options: {
-          caller: { name: 'cogs' },
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                corejs: '3.14',
-                targets: 'defaults',
-                useBuiltIns: 'usage'
-              }
-            ],
-            [
-              '@babel/preset-react',
-              { development: !MINIFY, runtime: 'automatic' }
-            ]
-          ]
-        }
-      },
-      {
-        name: 'concat-commonjs',
-        only: '**/*.+(js|json|svg)',
-        options: { entry: 'src/entry.js' }
-      },
-      MINIFY
-        ? {
-            name: 'terser',
-            only: '**/*.+(js|json|svg)',
-            except: '**/*+(-|_|.)min.js'
-          }
-        : [],
-      {
-        name: 'postcss',
-        only: 'src/index.css',
-        options: {
-          plugins: [
-            tailwindcss({
-              mode: 'jit',
-              purge: ['src/+(components|public)/**/*']
-            }),
-            autoprefixer
-          ]
-        }
-      },
-      {
-        only: 'src/index.css',
-        fn: ({ file: { links } }) => ({ links: [...links, 'src/**/*'] })
-      },
-      MINIFY ? { name: 'csso', only: 'src/index.css' } : []
-    ),
+const config = {
+  index: {
+    transformers: 'underscore-template',
     builds: {
-      'src/nginx.conf': { base: 'src', dir: '/usr/local/nginx/conf' },
-      'src/entry.js': { base: 'src', dir: 'dist' },
-      'src/index.html': { base: 'src', dir: 'dist' },
-      'src/index.css': { base: 'src', dir: 'dist' }
-    },
-    manifestPath: 'dist/manifest.json'
+      'src/index.html': { base: 'src', dir: 'dist' }
+    }
+  },
+  nginx: {
+    transformers: 'underscore-template',
+    builds: { 'src/nginx.conf': { base: 'src', dir: '/usr/local/nginx/conf' } }
   }
 };
+
+config.index.requires = 'main';
+
+config.public = {
+  builds: {
+    'src/public/**': { base: 'src/public', dir: 'dist' }
+  }
+};
+
+config.main = {
+  transformers: [].concat(
+    {
+      name: 'replace',
+      only: 'node_modules/**/*.js',
+      options: {
+        flags: 'g',
+        patterns: {
+          'process\\.env\\.NODE_ENV': MINIFY
+            ? '"production"'
+            : '"development"'
+        }
+      }
+    },
+    {
+      name: 'babel',
+      only: ['src/**/*.js'],
+      options: {
+        caller: { name: 'cogs' },
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              corejs: '3.14',
+              targets: 'defaults',
+              useBuiltIns: 'usage'
+            }
+          ],
+          [
+            '@babel/preset-react',
+            { development: !MINIFY, runtime: 'automatic' }
+          ]
+        ]
+      }
+    },
+    {
+      name: 'concat-commonjs',
+      only: '**/*.js',
+      options: { entry: 'src/entry.js' }
+    },
+    MINIFY
+      ? {
+          name: 'terser',
+          only: '**/*.js',
+          except: '**/*+(-|_|.)min.js'
+        }
+      : [],
+    {
+      name: 'postcss',
+      only: 'src/index.css',
+      options: {
+        plugins: [
+          tailwindcss({
+            mode: 'jit',
+            purge: ['src/+(components|public)/**/*']
+          }),
+          autoprefixer
+        ]
+      }
+    },
+    {
+      only: 'src/index.css',
+      fn: ({ file: { links } }) => ({ links: [...links, 'src/**/*'] })
+    },
+    MINIFY ? { name: 'csso', only: 'src/index.css' } : []
+  ),
+  builds: {
+    'src/entry.js': { base: 'src', dir: 'dist' },
+    'src/index.css': { base: 'src', dir: 'dist' }
+  },
+  manifestPath: 'dist/manifest.json'
+};
+
+export default config;
