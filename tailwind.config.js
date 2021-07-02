3a84292191f8f7151eb2cd22d488040749c4c3b9
nathanielop <nathaniel@galactivity.com>
2021-07-02 14:14:18 -0500

    tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ module.exports = {
   purge: {
     enabled: process.env.HUGO_ENVIRONMENT === 'production',
     content: [
-      'layouts/**/*.html',
-      'content/**/*.md',
-      'content/**/*.html',
+      './layouts/**/*.html',
+      './content/**/*.md',
+      './content/**/*.html',
     ],
   },
   darkMode: false, // or 'media' or 'class'
