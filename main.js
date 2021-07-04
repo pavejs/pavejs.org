2a051d7c83aae9c98ea6a26b574c5e60cd47d168
nathanielop <nathanielop@users.noreply.github.com>
2021-07-04 17:33:43 +0000

    deploy: 07c49310f8f7f18bf02df15e4a580d43c4d22374

--- main.js
+++ main.js
@@ -4,12 +4,15 @@
     const navGroups = document.querySelectorAll(".nav-group");
     navGroups.forEach((el) => {
       const controls = document.getElementById(el.dataset.controls);
-      console.log(el.dataset.controls);
+      const icon = document.getElementById(`${el.dataset.controls.slice(0, -8)}-indicator`);
       el.addEventListener("click", () => {
-        if (controls.classList.contains("flex"))
+        if (controls.classList.contains("flex")) {
           controls.classList.replace("flex", "hidden");
-        else
+          icon.classList.replace("rotate-90", "rotate-0");
+        } else {
           controls.classList.replace("hidden", "flex");
+          icon.classList.replace("rotate-0", "rotate-90");
+        }
       });
     });
   };
