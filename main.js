49388479144bab90e4983a390ef02057b288c7bb
nathanielop <nathanielop@users.noreply.github.com>
2021-07-04 17:28:35 +0000

    deploy: f29a8a60a77d922109782cf1f4a9f73f1e0d2738

--- main.js
+++ main.js
@@ -4,6 +4,7 @@
     const navGroups = document.querySelectorAll(".nav-group");
     navGroups.forEach((el) => {
       const controls = document.getElementById(el.dataset.controls);
+      console.log(el.dataset.controls);
       el.addEventListener("click", () => {
         if (controls.classList.contains("flex"))
           controls.classList.replace("flex", "hidden");
