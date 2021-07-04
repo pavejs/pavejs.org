f29a8a60a77d922109782cf1f4a9f73f1e0d2738
nathanielop <nathaniel@galactivity.com>
2021-07-04 12:28:03 -0500

    ugly debugging

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ window.onload = () => {
     const navGroups = document.querySelectorAll(".nav-group");
     navGroups.forEach(el => {
         const controls = document.getElementById(el.dataset.controls);
+        console.log(el.dataset.controls);
         el.addEventListener('click', () => {
             if (controls.classList.contains('flex')) controls.classList.replace('flex', 'hidden');
             else controls.classList.replace('hidden', 'flex');
