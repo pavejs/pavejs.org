d4ed17ebf322bce7277cb3dc52f417ba4660fd38
nathanielop <nathaniel@galactivity.com>
2021-07-04 11:53:26 -0500

    Add transform for bs-icons in config for svgs, work on making collapsible nav groups work

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+    const navGroups = querySelectorAll(".nav-group");
+    navGroups.forEach(el => {
+        const controls = document.getElementById(el.dataset.controls);
+        el.addEventListener('click', () => {
+            if (controls.classList.contains('flex')) controls.classList.replace('flex', 'hidden');
+            else controls.classList.replace('hidden', 'flex');
+        });
+    });
+}
