a5c56d08af250b5dc7f8acba2b268d7674157e3a
nathanielop <nathanielop@users.noreply.github.com>
2021-07-11 15:33:49 +0000

    deploy: b2cbac88b10e59ab87ad7ebd48899fc77083c6c2

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 (() => {
   // <stdin>
+  var focusSearch = () => {
+    const el = document.getElementById("docs-search");
+    el.focus();
+    scrollTo(0);
+  };
+  var BINDS = [
+    {keys: ["Ctrl", "/"], fn: focusSearch}
+  ];
   window.onload = () => {
+    const pressedKeys = new Set();
+    const watchedKeyGroups = BINDS.map(({keys}) => keys);
+    const watchedKeys = watchedKeyGroups.flat();
+    document.addEventListener("keydown", (e) => watchedKeys.indexOf(e.key) !== -1 && pressedKeys.add(e.key));
+    document.addEventListener("keyup", (e) => watchedKeys.indexOf(e.key) !== -1 && pressedKeys.delete(e.key));
+    watchedKeyGroups.map((keyArr) => {
+      keyArr.forEach((i) => {
+      });
+    });
   };
 })();
